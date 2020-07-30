608001cef458d84d5a01ef9c80a0aaeb44157ee4
zzy-day <2933287842@qq.com>
2020-07-30 10:10:16 +0800

    添加项目文件。

---
+++ 项目结构UI/Controllers/EchartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 项目结构UI.Controllers
+{
+    public class EchartController : Controller
+    {
+        // GET: Echart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
